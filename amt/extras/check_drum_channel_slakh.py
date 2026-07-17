231e1e71053c04b10e437c20cd5681464d8ef8c5
Benjie Genchel <benjiegenchel@gmail.com>
2026-07-17 18:00:43 +0900

    refactor of the package, to uv compatible, also restructuring of amt, removal of src directory.

---
+++ amt/extras/check_drum_channel_slakh.py
@@ -0,0 +1,23 @@
+import logging
+from amt.utils.mirdata_dev.datasets import slakh16k
+
+logger = logging.getLogger(__name__)
+
+
+def check_drum_channel_slakh(data_home: str):
+    ds = slakh16k.Dataset(data_home, version="default")
+    for track_id in ds.track_ids:
+        is_drum = ds.track(track_id).is_drum
+        midi = MidiFile(ds.track(track_id).midi_path)
+        cnt = 0
+        for msg in midi:
+            if "note" in msg.type:
+                if is_drum and (msg.channel != 9):
+                    logger.info('%s %s', "found drum track with channel != 9 in track_id: ", track_id)
+                if not is_drum and (msg.channel == 9):
+                    logger.info('%s %s', "found non-drum track with channel == 9 in track_id: ", track_id)
+                if is_drum and (msg.channel == 9):
+                    cnt += 1
+        if cnt > 0:
+            logger.info('%s %s', f"found {cnt} notes in drum track with ch 9 in track_id: ", track_id)
+    return
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-
+import logging
 import spaces
 
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'amt/src')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "amt/src")))
 
 import subprocess
 from typing import Tuple, Dict, Literal
@@ -18,39 +19,146 @@ import gradio as gr
 from gradio_log import Log
 from pathlib import Path
 
+logger = logging.getLogger(__name__)
+
 # gradio_log
-log_file = 'amt/log.txt'
+log_file = "amt/log.txt"
 Path(log_file).touch()
 
 # @title Load Checkpoint
-model_name = 'YPTF.MoE+Multi (noPS)' # @param ["YMT3+", "YPTF+Single (noPS)", "YPTF+Multi (PS)", "YPTF.MoE+Multi (noPS)", "YPTF.MoE+Multi (PS)"]
-precision = '16'# if torch.cuda.is_available() else '32'# @param ["32", "bf16-mixed", "16"]
-project = '2024'
+model_name = "YPTF.MoE+Multi (noPS)"  # @param ["YMT3+", "YPTF+Single (noPS)", "YPTF+Multi (PS)", "YPTF.MoE+Multi (noPS)", "YPTF.MoE+Multi (PS)"]
+precision = (
+    "16"  # if torch.cuda.is_available() else '32'# @param ["32", "bf16-mixed", "16"]
+)
+project = "2024"
 
 if model_name == "YMT3+":
     checkpoint = "notask_all_cross_v6_xk2_amp0811_gm_ext_plus_nops_b72@model.ckpt"
-    args = [checkpoint, '-p', project, '-pr', precision]
+    args = [checkpoint, "-p", project, "-pr", precision]
 elif model_name == "YPTF+Single (noPS)":
-    checkpoint = "ptf_all_cross_rebal5_mirst_xk2_edr005_attend_c_full_plus_b100@model.ckpt"
-    args = [checkpoint, '-p', project, '-enc', 'perceiver-tf', '-ac', 'spec',
-            '-hop', '300', '-atc', '1', '-pr', precision]
+    checkpoint = (
+        "ptf_all_cross_rebal5_mirst_xk2_edr005_attend_c_full_plus_b100@model.ckpt"
+    )
+    args = [
+        checkpoint,
+        "-p",
+        project,
+        "-enc",
+        "perceiver-tf",
+        "-ac",
+        "spec",
+        "-hop",
+        "300",
+        "-atc",
+        "1",
+        "-pr",
+        precision,
+    ]
 elif model_name == "YPTF+Multi (PS)":
     checkpoint = "mc13_256_all_cross_v6_xk5_amp0811_edr005_attend_c_full_plus_2psn_nl26_sb_b26r_800k@model.ckpt"
-    args = [checkpoint, '-p', project, '-tk', 'mc13_full_plus_256',
-            '-dec', 'multi-t5', '-nl', '26', '-enc', 'perceiver-tf',
-            '-ac', 'spec', '-hop', '300', '-atc', '1', '-pr', precision]
+    args = [
+        checkpoint,
+        "-p",
+        project,
+        "-tk",
+        "mc13_full_plus_256",
+        "-dec",
+        "multi-t5",
+        "-nl",
+        "26",
+        "-enc",
+        "perceiver-tf",
+        "-ac",
+        "spec",
+        "-hop",
+        "300",
+        "-atc",
+        "1",
+        "-pr",
+        precision,
+    ]
 elif model_name == "YPTF.MoE+Multi (noPS)":
-    checkpoint = "mc13_256_g4_all_v7_mt3f_sqr_rms_moe_wf4_n8k2_silu_rope_rp_b36_nops@last.ckpt"
-    args = [checkpoint, '-p', project, '-tk', 'mc13_full_plus_256', '-dec', 'multi-t5',
-            '-nl', '26', '-enc', 'perceiver-tf', '-sqr', '1', '-ff', 'moe',
-            '-wf', '4', '-nmoe', '8', '-kmoe', '2', '-act', 'silu', '-epe', 'rope',
-            '-rp', '1', '-ac', 'spec', '-hop', '300', '-atc', '1', '-pr', precision]
+    checkpoint = (
+        "mc13_256_g4_all_v7_mt3f_sqr_rms_moe_wf4_n8k2_silu_rope_rp_b36_nops@last.ckpt"
+    )
+    args = [
+        checkpoint,
+        "-p",
+        project,
+        "-tk",
+        "mc13_full_plus_256",
+        "-dec",
+        "multi-t5",
+        "-nl",
+        "26",
+        "-enc",
+        "perceiver-tf",
+        "-sqr",
+        "1",
+        "-ff",
+        "moe",
+        "-wf",
+        "4",
+        "-nmoe",
+        "8",
+        "-kmoe",
+        "2",
+        "-act",
+        "silu",
+        "-epe",
+        "rope",
+        "-rp",
+        "1",
+        "-ac",
+        "spec",
+        "-hop",
+        "300",
+        "-atc",
+        "1",
+        "-pr",
+        precision,
+    ]
 elif model_name == "YPTF.MoE+Multi (PS)":
-    checkpoint = "mc13_256_g4_all_v7_mt3f_sqr_rms_moe_wf4_n8k2_silu_rope_rp_b80_ps2@model.ckpt"
-    args = [checkpoint, '-p', project, '-tk', 'mc13_full_plus_256', '-dec', 'multi-t5',
-            '-nl', '26', '-enc', 'perceiver-tf', '-sqr', '1', '-ff', 'moe',
-            '-wf', '4', '-nmoe', '8', '-kmoe', '2', '-act', 'silu', '-epe', 'rope',
-            '-rp', '1', '-ac', 'spec', '-hop', '300', '-atc', '1', '-pr', precision]
+    checkpoint = (
+        "mc13_256_g4_all_v7_mt3f_sqr_rms_moe_wf4_n8k2_silu_rope_rp_b80_ps2@model.ckpt"
+    )
+    args = [
+        checkpoint,
+        "-p",
+        project,
+        "-tk",
+        "mc13_full_plus_256",
+        "-dec",
+        "multi-t5",
+        "-nl",
+        "26",
+        "-enc",
+        "perceiver-tf",
+        "-sqr",
+        "1",
+        "-ff",
+        "moe",
+        "-wf",
+        "4",
+        "-nmoe",
+        "8",
+        "-kmoe",
+        "2",
+        "-act",
+        "silu",
+        "-epe",
+        "rope",
+        "-rp",
+        "1",
+        "-ac",
+        "spec",
+        "-hop",
+        "300",
+        "-atc",
+        "1",
+        "-pr",
+        precision,
+    ]
 else:
     raise ValueError(model_name)
 
@@ -59,42 +167,66 @@ model.to("cuda")
 # @title GradIO helper
 
 
-def prepare_media(source_path_or_url: os.PathLike,
-                  source_type: Literal['audio_filepath', 'youtube_url'],
-                  delete_video: bool = True,
-                  simulate = False) -> Dict:
+def prepare_media(
+    source_path_or_url: os.PathLike,
+    source_type: Literal["audio_filepath", "youtube_url"],
+    delete_video: bool = True,
+    simulate=False,
+) -> Dict:
     """prepare media from source path or youtube, and return audio info"""
     # Get audio_file
-    if source_type == 'audio_filepath':
+    if source_type == "audio_filepath":
         audio_file = source_path_or_url
-    elif source_type == 'youtube_url':
-        if os.path.exists('/download/yt_audio.mp3'):
-            os.remove('/download/yt_audio.mp3')
+    elif source_type == "youtube_url":
+        if os.path.exists("/download/yt_audio.mp3"):
+            os.remove("/download/yt_audio.mp3")
         # Download from youtube
-        with open(log_file, 'w') as lf:
-            audio_file = './downloaded/yt_audio'
-            command = ['yt-dlp', '-x', source_path_or_url, '-f', 'bestaudio',
-                '-o', audio_file, '--audio-format', 'mp3', '--restrict-filenames',
-                '--extractor-retries', '10',
-                '--force-overwrites', '--username', 'oauth2', '--password', '', '-v']
+        with open(log_file, "w") as lf:
+            audio_file = "./downloaded/yt_audio"
+            command = [
+                "yt-dlp",
+                "-x",
+                source_path_or_url,
+                "-f",
+                "bestaudio",
+                "-o",
+                audio_file,
+                "--audio-format",
+                "mp3",
+                "--restrict-filenames",
+                "--extractor-retries",
+                "10",
+                "--force-overwrites",
+                "--username",
+                "oauth2",
+                "--password",
+                "",
+                "-v",
+            ]
             if simulate:
-                command = command + ['-s']
-            process = subprocess.Popen(command,
-                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        
-            for line in iter(process.stdout.readline, ''):
+                command = command + ["-s"]
+            process = subprocess.Popen(
+                command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+            )
+
+            for line in iter(process.stdout.readline, ""):
                 # Filter out unnecessary messages
-                print(line)
+                logger.info("%s", line.rstrip())
                 if "www.google.com/device" in line:
-                    hl_text = line.replace("https://www.google.com/device", "\033[93mhttps://www.google.com/device\x1b[0m").split()
+                    hl_text = line.replace(
+                        "https://www.google.com/device",
+                        "\033[93mhttps://www.google.com/device\x1b[0m",
+                    ).split()
                     hl_text[-1] = "\x1b[31;1m" + hl_text[-1] + "\x1b[0m"
-                    lf.write(' '.join(hl_text)); lf.flush()
+                    lf.write(" ".join(hl_text))
+                    lf.flush()
                 elif "Authorization successful" in line or "Video unavailable" in line:
-                    lf.write(line); lf.flush()
+                    lf.write(line)
+                    lf.flush()
             process.stdout.close()
             process.wait()
-        
-        audio_file += '.mp3'
+
+        audio_file += ".mp3"
     else:
         raise ValueError(source_type)
 
@@ -102,23 +234,25 @@ def prepare_media(source_path_or_url: os.PathLike,
     info = torchaudio.info(audio_file)
     return {
         "filepath": audio_file,
-        "track_name": os.path.basename(audio_file).split('.')[0],
+        "track_name": os.path.basename(audio_file).split(".")[0],
         "sample_rate": int(info.sample_rate),
         "bits_per_sample": int(info.bits_per_sample),
         "num_channels": int(info.num_channels),
         "num_frames": int(info.num_frames),
         "duration": int(info.num_frames / info.sample_rate),
         "encoding": str.lower(info.encoding),
-        }
+    }
+
 
 @spaces.GPU
 def process_audio(audio_filepath):
     if audio_filepath is None:
         return None
-    audio_info = prepare_media(audio_filepath, source_type='audio_filepath')
+    audio_info = prepare_media(audio_filepath, source_type="audio_filepath")
     midifile = transcribe(model, audio_info)
     midifile = to_data_url(midifile)
-    return create_html_from_midi(midifile) # html midiplayer
+    return create_html_from_midi(midifile)  # html midiplayer
+
 
 # This is a temporary function for using pre-transcribed midi
 @spaces.GPU
@@ -132,30 +266,34 @@ def process_audio_yt_temp(youtube_url):
     elif youtube_url == "https://youtu.be/OXXRoa1U6xU?si=dpYMun4LjZHNydSb":
         midifile = "./mid/Mozart_Sonata_for_Piano_and_Violin_(getmp3.pro).mid"
     midifile = to_data_url(midifile)
-    return create_html_from_midi(midifile) # html midiplayer
+    return create_html_from_midi(midifile)  # html midiplayer
 
 
 @spaces.GPU
 def process_video(youtube_url):
-    if 'youtu' not in youtube_url:
+    if "youtu" not in youtube_url:
         return None
-    audio_info = prepare_media(youtube_url, source_type='youtube_url')
+    audio_info = prepare_media(youtube_url, source_type="youtube_url")
     midifile = transcribe(model, audio_info)
     midifile = to_data_url(midifile)
-    return create_html_from_midi(midifile) # html midiplayer
+    return create_html_from_midi(midifile)  # html midiplayer
+
 
 def play_video(youtube_url):
-    if 'youtu' not in youtube_url:
+    if "youtu" not in youtube_url:
         return None
     return create_html_youtube_player(youtube_url)
+
 
 # def oauth_google():
 #     return create_html_oauth()
 
-AUDIO_EXAMPLES = glob.glob('examples/*.*', recursive=True)
-YOUTUBE_EXAMPLES = ["https://youtu.be/5vJBhdjvVcE?si=s3NFG_SlVju0Iklg",
-                    "https://youtu.be/mw5VIEIvuMI?si=Dp9UFVw00Tl8CXe2",
-                    "https://youtu.be/OXXRoa1U6xU?si=dpYMun4LjZHNydSb"]
+AUDIO_EXAMPLES = glob.glob("examples/*.*", recursive=True)
+YOUTUBE_EXAMPLES = [
+    "https://youtu.be/5vJBhdjvVcE?si=s3NFG_SlVju0Iklg",
+    "https://youtu.be/mw5VIEIvuMI?si=Dp9UFVw00Tl8CXe2",
+    "https://youtu.be/OXXRoa1U6xU?si=dpYMun4LjZHNydSb",
+]
 # YOUTUBE_EXAMPLES = ["https://youtu.be/5vJBhdjvVcE?si=s3NFG_SlVju0Iklg",
 #                     "https://www.youtube.com/watch?v=vMboypSkj3c",
 #                     "https://youtu.be/vRd5KEjX8vw?si=b-qw633ZjaX6Uxy5",
@@ -164,16 +302,16 @@ YOUTUBE_EXAMPLES = ["https://youtu.be/5vJBhdjvVcE?si=s3NFG_SlVju0Iklg",
 #                     "https://youtu.be/mIWYTg55h10?si=WkbtKfL6NlNquvT8"]
 
 theme = gr.Theme.from_hub("gradio/dracula_revamped")
-theme.text_md = '10px'
-theme.text_lg = '12px'
+theme.text_md = "10px"
+theme.text_lg = "12px"
 
-theme.body_background_fill_dark = '#060a1c' #'#372037'# '#a17ba5' #'#73d3ac'
-theme.border_color_primary_dark = '#45507328'
-theme.block_background_fill_dark = '#3845685c'
+theme.body_background_fill_dark = "#060a1c"  #'#372037'# '#a17ba5' #'#73d3ac'
+theme.border_color_primary_dark = "#45507328"
+theme.block_background_fill_dark = "#3845685c"
 
-theme.body_text_color_dark = 'white'
-theme.block_title_text_color_dark = 'black'
-theme.body_text_color_subdued_dark = '#e4e9e9'
+theme.body_text_color_dark = "white"
+theme.block_title_text_color_dark = "black"
+theme.body_text_color_subdued_dark = "#e4e9e9"
 
 css = """
 .gradio-container {
@@ -194,8 +332,7 @@ with gr.Blocks(theme=theme, css=css) as demo:
 
     with gr.Row():
         with gr.Column(scale=10):
-            gr.Markdown(
-            f"""
+            gr.Markdown(f"""
             ## 🎶YourMT3+: Multi-instrument Music Transcription with Enhanced Transformer Architectures and Cross-dataset Stem Augmentation
             - Model name: `{model_name}`
                 <details>
@@ -239,43 +376,60 @@ with gr.Blocks(theme=theme, css=css) as demo:
         with gr.Tab("From YouTube"):
             with gr.Column(scale=4):
                 # Input URL
-                youtube_url = gr.Textbox(label="YouTube Link URL",
-                        placeholder="https://youtu.be/...")
+                youtube_url = gr.Textbox(
+                    label="YouTube Link URL", placeholder="https://youtu.be/..."
+                )
                 # Display examples
                 gr.Examples(examples=YOUTUBE_EXAMPLES, inputs=youtube_url)
                 # Play button
-                play_video_button = gr.Button("Get Audio from YouTube", variant="primary")
+                play_video_button = gr.Button(
+                    "Get Audio from YouTube", variant="primary"
+                )
                 # Play youtube
                 youtube_player = gr.HTML(render=True)
 
             with gr.Column(scale=4):
-                    with gr.Row():
-                        # Submit button
-                        transcribe_video_button = gr.Button("Transcribe", variant="primary")
-                        # Oauth button
-                        oauth_button = gr.Button("google.com/device", variant="primary", link="https://www.google.com/device")
-                    
+                with gr.Row():
+                    # Submit button
+                    transcribe_video_button = gr.Button("Transcribe", variant="primary")
+                    # Oauth button
+                    oauth_button = gr.Button(
+                        "google.com/device",
+                        variant="primary",
+                        link="https://www.google.com/device",
+                    )
+
             with gr.Column(scale=1):
                 # Transcribe
                 output_tab2 = gr.HTML(render=True)
                 # video_output = gr.Text(label="Video Info")
-                transcribe_video_button.click(process_audio_yt_temp, inputs=youtube_url, outputs=output_tab2)
+                transcribe_video_button.click(
+                    process_audio_yt_temp, inputs=youtube_url, outputs=output_tab2
+                )
                 # transcribe_video_button.click(process_video, inputs=youtube_url, outputs=output_tab2)
                 # Play
-                play_video_button.click(play_video, inputs=youtube_url, outputs=youtube_player)
+                play_video_button.click(
+                    play_video, inputs=youtube_url, outputs=youtube_player
+                )
             with gr.Column(scale=1):
-                Log(log_file, dark=True, xterm_font_size=12, elem_id='mylog')
+                Log(log_file, dark=True, xterm_font_size=12, elem_id="mylog")
 
         with gr.Tab("Upload audio"):
             # Input
-            audio_input = gr.Audio(label="Record Audio", type="filepath",
-                                show_share_button=True, show_download_button=True)
+            audio_input = gr.Audio(
+                label="Record Audio",
+                type="filepath",
+                show_share_button=True,
+                show_download_button=True,
+            )
             # Display examples
             gr.Examples(examples=AUDIO_EXAMPLES, inputs=audio_input)
             # Submit button
             transcribe_audio_button = gr.Button("Transcribe", variant="primary")
             # Transcribe
             output_tab1 = gr.HTML()
-            transcribe_audio_button.click(process_audio, inputs=audio_input, outputs=output_tab1)
+            transcribe_audio_button.click(
+                process_audio, inputs=audio_input, outputs=output_tab1
+            )
 
 demo.launch(debug=True)
